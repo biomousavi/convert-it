46a61e16adf090a33261644f6169d2b04baf880e
Mohammad hosein Mousavi <biomousavi@gmail.com>
2023-01-28 16:46:39 +0330

    chore: add vuetify conf for nuxt 3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,2 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({});
+import vuetify from 'vite-plugin-vuetify';
+
+export default defineNuxtConfig({
+  build: {
+    transpile: ['vuetify'],
+  },
+  modules: [
+    // Vuetify vite treeshaking plugin
+    (_, nuxt) => {
+      // @ts-ignore
+      nuxt.hooks.hook('vite:extendConfig', (config) => config?.plugins.push(vuetify()));
+    },
+  ],
+});
