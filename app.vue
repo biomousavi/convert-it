8baa2d21bb74877a587f5c409d9f11f9a4c313e7
Mohammad hosein Mousavi <biomousavi@gmail.com>
2023-01-28 18:39:35 +0330

    style: add background gradient

--- app.vue
+++ app.vue
@@ -1,5 +1,20 @@
 <template>
   <VApp>
     <AppHead />
+
+    <div class="page"></div>
   </VApp>
 </template>
+
+<style>
+::-webkit-scrollbar {
+  width: 0;
+  height: 0;
+}
+
+.page {
+  min-width: 100%;
+  min-height: 100vh;
+  background: linear-gradient(135deg, #34568b 0%, #6b5b95 100%);
+}
+</style>
